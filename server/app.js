21418259cf17aa11e7d01244bb602dab7628d6eb
quocthinh2609 <65326420+quocthinh2609@users.noreply.github.com>
2020-12-09 17:54:26 +0700

    Kết nối server đến database

--- server/app.js
+++ server/app.js
@@ -1,10 +1,28 @@
 const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+    cors({
+      origin: "*",
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    })
+  );
 
 app.get("/", ( req, res ) => {
     res.status(200).send("This is server :)");
 })
+
+app.use('/account', require('./modules/account/account.routes'));
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`server is running on port ${process.env.PORT}`)
