d1af73af841d2fba1102bb607a3f0df148ec9385
quocthinh2609 <65326420+quocthinh2609@users.noreply.github.com>
2020-12-12 15:52:26 +0700

    Khởi tạo Global Error Handler.

--- server/app.js
+++ server/app.js
@@ -22,7 +22,18 @@ app.get("/", ( req, res ) => {
     res.status(200).send("This is server :)");
 })
 
-app.use('/account', require('./modules/account/account.routes'));
+app.use('/api/account', require("./modules/account/account.routes"));
+
+
+//Global Error Handler
+app.use((err, req, res, next) => {
+  res.status( err.httpCode || 400 ).send({
+    code: err.httpCode || 400,
+    error: err.error === undefined ? true : err.error,
+    message: err.message || `Something broken !! Reported By Express Built-in Error Handler \n ${JSON.stringify(err)}`,
+    data: err.data || null,
+  });
+})
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`server is running on port ${process.env.PORT}`)
