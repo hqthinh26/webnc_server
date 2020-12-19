0c34b7b9f684fd5808b399eef8b8182f8d377e5b
Thinhhq <quocthinh.huynh26@gmail.com>
2020-12-19 11:15:09 +0700

    + Thêm middleware - Kiểm tra token valid & user có tồn tại

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,3 +1,28 @@
-// exports.isAuthenticated = (req, res, next) => {
+const responseHanlder = require("../response_handler");
+const jwt = require("jsonwebtoken");
 
-// }
+const authMessages = {
+  tokenIsEmpty: "token is required",
+  tokenIsNotValid: "Token is not valid",
+};
+
+exports.isAuthenticate = async (req, res, next) => {
+  // must always be 2 token | access_token && refresh_token
+  try {
+    const authorization = req.headers["authorization"];
+    if (!authorization) return next(responseHanlder.error(authMessages.tokenIsEmpty));
+
+    const access_token = authorization.split(' ')[1];
+
+    if (!access_token) return next(responseHanlder.error(authMessages.tokenIsNotValid));
+
+    const payload = await jwt.verify(
+      access_token,
+      process.env.SUPER_SECRET_KEY
+    );
+
+    next();
+  } catch (e) {
+    next(e);
+  }
+};
