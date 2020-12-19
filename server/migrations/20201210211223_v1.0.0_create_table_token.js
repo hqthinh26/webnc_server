515e513dcf202d612a741c18839c34b2cdfaceb6
Thinhhq <quocthinh.huynh26@gmail.com>
2020-12-19 11:18:04 +0700

    Bỏ table refresh_token => Chuyển thành table token (Chuyển từ từ 2 tokens thành 1 token)

--- server/migrations/20201210211223_v1.0.0_create_table_token.js
+++ server/migrations/20201210211223_v1.0.0_create_table_token.js
@@ -1,12 +1,12 @@
 
 exports.up = async function(knex) {
   return await knex.raw(`
-    CREATE TABLE refresh_token(
+    CREATE TABLE token(
         id bigserial PRIMARY KEY,
         account_id int8 NOT NULL,
         token text NOT NULL,
+        expired_at timestamp NOT NULL,
         logout_at timestamp,
-        expired_at timestamp,
         created_at timestamp default now(),
         updated_at timestamp default now(),
         CONSTRAINT fk_account_id
