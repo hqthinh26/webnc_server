216a84b30798f76598e873714798c0dfc8a164c5
Huỳnh Quốc Thịnh <quocthinh.huynh26@gmail.com>
2020-12-30 23:18:04 +0700

    Update tên khoá học

--- server/migrations/20201230184821_v1.0.1_populate_course_table.js
+++ server/migrations/20201230184821_v1.0.1_populate_course_table.js
@@ -6,7 +6,7 @@ exports.up = async function(knex) {
   ('A Complete Beginner Guide to JavaScript', 1, 4, 'https://miro.medium.com/max/2562/1*_Xqei5CDKIPJ7VewFuAPjA.png', '119.99', 3.2, 150000, false ),
   ('Learn Python Programming Masterclass', 2, 3, 'https://datawider.com/wp-content/uploads/2019/11/How-to-Learn-Python.jpg', '99.99', 4.5, 56321, false ),
   ('2020 Complete C++ Bootcamp From Zero to Hero in C++', 2, 3, 'https://engineering.fb.com/wp-content/uploads/2015/06/1522635669452_11.jpg', '112.99', 3.7, 87654, true ),
-  ('Entrance level: A Guide to Python & Django', 1, 3, 'https://lh6.googleusercontent.com/rU8dZ0x67y63AeujFhM79UG_I3ZagEqTmDffagrUVuBI5eXPHtW2Z7zP1KU1MLKtl0wU5eNS_QHU-9v3GUJgxlKYeAR1yKADY8xCj7xMrpL8z9Rr2Zde9_OGsmXTigvBr7DEWggV', '89.99', 2.8, 98767, false ),
+  ('Entrance level: A Guide to Python & Django Latest 2020', 1, 3, 'https://lh6.googleusercontent.com/rU8dZ0x67y63AeujFhM79UG_I3ZagEqTmDffagrUVuBI5eXPHtW2Z7zP1KU1MLKtl0wU5eNS_QHU-9v3GUJgxlKYeAR1yKADY8xCj7xMrpL8z9Rr2Zde9_OGsmXTigvBr7DEWggV', '89.99', 2.8, 98767, false ),
   ('NoSQL Database: MongoDB Guide', 1, 4, 'https://www.filepicker.io/api/file/BPuxT72TDetZjNxyG4gh', '125.99', 4.5, 765434, true );
   `);
 };
